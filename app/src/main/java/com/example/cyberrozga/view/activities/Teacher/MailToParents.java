75d84c87376d2253e31e1225af0bdd85ef83e7ee
idabrows <daiz@op.pl>
2019-02-07 20:55:52 +0100

    Design improvements

--- app/src/main/java/com/example/cyberrozga/view/activities/Teacher/MailToParents.java
+++ app/src/main/java/com/example/cyberrozga/view/activities/Teacher/MailToParents.java
@@ -2,6 +2,10 @@ package com.example.cyberrozga.view.activities.Teacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.cyberrozga.R;
 
@@ -11,5 +15,6 @@ public class MailToParents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_to_parents);
+
     }
 }
