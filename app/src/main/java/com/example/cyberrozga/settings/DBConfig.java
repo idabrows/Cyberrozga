032957e0102e0cbc06fbc72c5b2d47ddb6c359cb
Matmozaur <matmozaur@gmial.com>
2019-02-11 21:21:43 +0100

    final changes for now

--- app/src/main/java/com/example/cyberrozga/settings/DBConfig.java
+++ app/src/main/java/com/example/cyberrozga/settings/DBConfig.java
@@ -1,7 +1,7 @@
 package com.example.cyberrozga.settings;
 
 public interface DBConfig {
-    //For your settings
+    //Your settings
     public static final String myURL="//localhost:3306/cyberrozga";
     public static final String myLogin="root";
     public static final String myPassword="root";
