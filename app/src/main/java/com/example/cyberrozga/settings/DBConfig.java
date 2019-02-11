35a71eaa1646081d825cc6b8439e44724828c4f5
Maciej Dobrzański <mdobrzan@student.agh.edu.pl>
2019-02-11 21:35:07 +0100

    Update

--- app/src/main/java/com/example/cyberrozga/settings/DBConfig.java
+++ app/src/main/java/com/example/cyberrozga/settings/DBConfig.java
@@ -1,7 +1,6 @@
 package com.example.cyberrozga.settings;
 
 public interface DBConfig {
-    //Your credentials
     public static final String myURL="//localhost:3306/cyberrozga";
     public static final String myLogin="root";
     public static final String myPassword="";
